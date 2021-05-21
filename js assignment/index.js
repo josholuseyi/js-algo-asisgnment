a3fd5619fc3bcf55b59485e4603b66f64014a280
josholuseyi <73930446+josholuseyi@users.noreply.github.com>
2021-05-21 20:43:12 +0100

    Add files via upload

---
+++ js assignment/index.js
@@ -0,0 +1,19 @@
+function convertFahrToCelsius(fahrenheitToCelsius) 
+{
+  var temp = fahrenheit;
+  var FahrToCelsius = C = (F - 32) * 5/9;
+  var fToC = FahrToCelsius.toFixed(4);
+  var convertFahrToCelsius = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    if (convertfahrToCelsius === ""  ) {
+        console.log(convertFahrToCelsius)        
+    } else {
+        console.log("This is not a valid number but an array")
+    } if (convertFahrToCelsius === '') {
+        
+    } else {
+        console.log('[1,2,3] is not a valid number but an array')
+        
+    } {
+
+    }
+    }
